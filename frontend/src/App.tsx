b33559a6bd64e446ab9a498a35842e3cbbd6c35e
Rikjimue <rikjimue@gmail.com>
2024-10-09 15:02:16 -0400

    Added routes to pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
+import { Home } from './pages/Home'
+import { Navbar } from './components/Navbar'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+import Pricing from './pages/Pricing'
+import Login from './pages/Login'
+import Contact from './pages/Contact'
 
 function App() {
   return (
@@ -10,7 +15,11 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
-            {/* Add more routes here as you create more pages */}
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
       </div>
